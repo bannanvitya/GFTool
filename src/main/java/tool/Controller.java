23f84a600d1576af674ed6173efce7089c9101e6
VKHozhaynov <vithozh410@gmail.com>
2015-01-21 19:07:02 +0300

    First commit

---
+++ src/main/java/tool/Controller.java
@@ -0,0 +1,32 @@
+package tool;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+    @FXML public Button button1;
+    @FXML public TextField hostField;
+
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+
+
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                hostField.setText("FUCK!");
+            }
+        });
+
+    }
+
+}
